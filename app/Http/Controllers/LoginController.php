bc2c1c6ecad85a9b9b0614058fb508173b02b56d
Mohamed Hesham <mohamedhesham011010@gmail.com>
2024-02-28 20:05:03 +0200

    laravel ui with login auth

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function home()
+    {
+
+        if (Auth::id()) {
+            if (!auth()->user()->roles->isempty()) {
+                $user_role = auth()->user()->roles->first()->name;
+                if ($user_role == '') {
+                    return view('');
+                }
+            }
+            return view('user.home');
+        }
+    }
+    public function checkLogin(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('');
+        }
+        return redirect()->back()->withErrors(['Error' => 'Invalid Email or Password']);
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->back();
+    }
+}
